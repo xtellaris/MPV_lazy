9af5b4050286041603ed5382d11ec0931cf2ea69
hooke007 <41094733+hooke007@users.noreply.github.com>
2021-02-23 12:57:31 +0800

    转移

--- _奇奇怪怪的姿势/test.bat
+++ _奇奇怪怪的姿势/test.bat
@@ -1,31 +1,31 @@
-:: ˵����UTF8����������޸ı���ΪANSI��GB18030��
+:: 说明（UTF8中文乱码可修改编码为ANSI或GB18030）
 @echo off
 echo ==========================================================
-echo == ����ģʽ�����ڼ��ݲ��Ի�������;
-echo ��ǰĿ¼��������mpv.com�����һ�������Path�еĴ���
-echo �Խӽ�ԭʼĬ�ϲ����Ĵ�ͳ��ʽ����ԭ��MPV
-echo ԭ��MPV���ڴ򿪺�Ҫ��CMD�ڽ��в���
-echo �رո�CMD����Ҳ���Զ��ر�MPV
+echo == 纯净模式可用于兼容测试或特殊用途
+echo 当前目录若不存在mpv.com将查找环境变量Path中的代替
+echo 以接近原始默认参数的传统方式启动原版MPV
+echo 原版MPV窗口打开后不要在CMD内进行操作
+echo 关闭该CMD窗口也将自动关闭MPV
 
-:: �����ִ�еĲ�����Ϣ
+:: 输出将执行的参数信息
 echo ==========================================================
-echo == ��������
+echo == 启动参数
 echo mpv --config=no --idle=once --force-window=yes --keep-open=yes --hidpi-window-scale=no --autofit-smaller=500x500
 echo ==========================================================
-echo == ����Ϊ��־
+echo == 以下为日志
 echo ==========================================================
 
-:: ���ĵ�ǰ����Ŀ¼
+:: 更改当前工作目录
 cd /D %~dp0
 
-:: ����汾��Ϣ
+:: 输出版本信息
 mpv.com --version
 
-:: ִ����������
+:: 执行启动参数
 mpv.com --config=no --idle=once --force-window=yes --keep-open=yes --hidpi-window-scale=no --autofit-smaller=500x500
 
-:: �ֶ��˳���������ı�
+:: 手动退出后输出的文本
 echo ==========================================================
-echo == MPV�ѹر�
+echo == MPV已关闭
 echo ==========================================================
 pause
