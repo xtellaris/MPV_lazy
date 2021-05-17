a3286b27d3bc45e61146dec196d3a8dfe5ad7543
hooke007 <41094733+hooke007@users.noreply.github.com>
2021-05-17 17:22:21 +0800

    规范化命名

--- scripts/ontop_playback.lua
+++ scripts/ontop_playback.lua
@@ -1,5 +1,5 @@
 --[[
-source: https://github.com/mpv-player/mpv/blob/master/TOOLS/lua/ontop-playback.lua
+Source: https://github.com/mpv-player/mpv/blob/master/TOOLS/lua/ontop-playback.lua
 使用此脚本后 --ontop 仅在播放状态时启用置顶，暂停自动取消置顶
 同功能的 on_top_only_while_playing.lua 脚本存在以暂停状态启动mpv时无法识别的问题
 --]]
