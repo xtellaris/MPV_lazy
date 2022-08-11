dfacd8cabeec4b2ce6c7e4b2968a0a452a291321
hooke007 <hooke007@qq.com>
2022-08-11 23:43:41 +0100

    记录属性脚本：支持自定义缓存路径

--- portable_config/scripts/save_global_props.lua
+++ portable_config/scripts/save_global_props.lua
@@ -3,7 +3,7 @@
 SOURCE_ https://github.com/zenwarr/mpv-config/blob/master/scripts/remember-props.lua
 COMMIT_ 20220811 03cfc0e39682a73d9d24a6e01a3c02716a019d1d
 
-记录全局的属性变化，支持在下次程序启动时恢复，其对应数据保存在设置目录下的文件 saved-props.json
+记录全局的属性变化，支持在下次程序启动时恢复，其对应数据保存在对应文件 saved-props.json
 （选项 --save-position-on-quit 保存的是基于具体文件的属性，不要与 --watch-later-options 保存的属性相冲突）
 
 示例在 input.conf 中写入（清除已记录的数据）：
@@ -14,9 +14,6 @@ CTRL+r script-message-to save_global_props clean_data
 local mp = require("mp")
 local options = require("mp.options")
 local utils = require("mp.utils")
-
-
-local data_file_path = (mp.command_native({'expand-path', '~~/saved-props.json'}))
 
 
 local function split(inputstr, sep)
@@ -31,10 +28,14 @@ end
 
 
 local script_options = {
-    props = "volume,speed"
+    props     = "volume,mute,speed",   -- 自定义要记录的属性
+    cache_dir = "~~/"                  -- 缓存文件的路径
 }
 options.read_options(script_options)
 script_options.props = split(script_options.props, ",")
+
+
+local data_file_path = (mp.command_native({'expand-path', script_options.cache_dir .. "saved-props.json"}))
 
 
 local function read_data_file()
@@ -77,6 +78,7 @@ local function clean_data_file()
     file:write(content)
 
     file:close()
+    mp.osd_message("已清理记录的属性", 3)
 end
 
 
