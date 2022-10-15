c17cbb909b843bcb92b5ffb553ff3be02855c932
hooke007 <hooke007@qq.com>
2022-10-15 19:29:50 +0100

    脚本： input_plus 扩充更多命令
    原 load_plus 中的 双音轨同步播放功能 迁移至 input_plus ；
    增加 单键触发 双音轨同步播放；
    增加 子弹时间；
    增加 单键浏览统计数据；
    增加 切换旧版x264即时兼容模式；
    扩展 播放列表洗牌；
    简化 部分函数。
    
    精简 input.conf 中的示例。

--- portable_config/scripts/input_plus.lua
+++ portable_config/scripts/input_plus.lua
@@ -4,39 +4,152 @@
 
 input.conf 示例：
 
-Alt+p              script-binding input_plus/playlist_shuffle   # 播放列表的洗牌与撤销
-CLOSE_WIN          script-binding input_plus/quit_real          # 对执行退出命令前的确认（防止误触）
-q                  script-binding input_plus/quit_wait          # 延后退出命令的执行（执行前再次触发可取消）
-Alt+LEFT           script-binding input_plus/seek_auto_back     # 后退至上句字幕的时间点或上一关键帧
-Alt+RIGHT          script-binding input_plus/seek_auto_next     # 前进至下...
-Shift+Ctrl+LEFT    script-binding input_plus/seek_skip_back     # 向后大跳（精确帧）
-Shift+Ctrl+RIGHT   script-binding input_plus/seek_skip_next     # 向前...
-SPACE              script-binding input_plus/speed_auto         # 按下两倍速，松开一倍速
-BS                 script-binding input_plus/speed_recover      # 仿Pot的速度重置与恢复
-1                  script-binding input_plus/trackA_back        # 上一个音频轨道（自动跳过无轨道）
-2                  script-binding input_plus/trackA_next        # 下...
-3                  script-binding input_plus/trackS_back        # 上一个字幕轨道...
-4                  script-binding input_plus/trackS_next        # 下...
-5                  script-binding input_plus/trackV_back        # 上一个视频轨道...
-6                  script-binding input_plus/trackV_next        # 下...
+ #                    script-binding input_plus/mark_aid_A          # 标记当前音轨为A
+ #                    script-binding input_plus/mark_aid_B          # 标记当前音轨为B
+ #                    script-binding input_plus/mark_aid_merge      # 合并AB音轨
+ #                    script-binding input_plus/mark_aid_reset      # 取消AB并轨和标记
+ m                    script-binding input_plus/mark_aid_fin        # （单键实现上述四项命令）
+
+ Alt+p                script-binding input_plus/playlist_order_0    # 播放列表的洗牌与撤销
+ #                    script-binding input_plus/playlist_order_0r   # ...（重定向至首个文件）
+ #                    script-binding input_plus/playlist_order_1    # 播放列表连续洗牌（可用上两项命令恢复）
+ #                    script-binding input_plus/playlist_order_1r   # ...
+
+ CLOSE_WIN            script-binding input_plus/quit_real           # 对执行退出命令前的确认（防止误触）
+ #                    script-binding input_plus/quit_wait           # 延后退出命令的执行（执行前再次触发可取消）
+
+ #                    script-binding input_plus/seek_auto_back      # [可持续触发] 后退至上句字幕的时间点或上一关键帧
+ #                    script-binding input_plus/seek_auto_next      # [可持续触发] 前进至下...
+ #                    script-binding input_plus/seek_skip_back      # 向后大跳（精确帧）
+ #                    script-binding input_plus/seek_skip_next      # 向前...
+
+ SPACE                script-binding input_plus/speed_auto          # [按住/松开] 两倍速/一倍速
+ #                    script-binding input_plus/speed_auto_bullet   # [按住/松开] 子弹时间/一倍速
+ #                    script-binding input_plus/speed_recover       # 仿Pot的速度重置与恢复
+
+ #                    script-binding input_plus/stats_1_2           # 单键浏览统计数据第1至2页
+ #                    script-binding input_plus/stats_0_4           # 单键浏览统计数据第0至4页
+
+ #                    script-binding input_plus/trackA_back         # 上一个音频轨道（自动跳过无轨道）
+ #                    script-binding input_plus/trackA_next         # 下...
+ #                    script-binding input_plus/trackS_back         # 上一个字幕轨道...
+ #                    script-binding input_plus/trackS_next         # 下...
+ #                    script-binding input_plus/trackV_back         # 上一个视频轨道...
+ #                    script-binding input_plus/trackV_next         # 下...
+
+ #                    script-binding input_plus/x264_cmpt           # 开/关 旧版x264即时兼容模式
 
 --]]
 
 
+local marked_aid_A = nil
+local marked_aid_B = nil
+local merged_aid = false
+function mark_aid_A()
+	marked_aid_A = mp.get_property_number("aid", 0)
+	if marked_aid_A == 0
+	then
+		mp.osd_message("当前音轨无效", 1)
+		marked_aid_A = nil
+	else
+		mp.osd_message("预标记当前音轨序列 " .. marked_aid_A .. " 为并行轨A", 1)
+	end
+end
+function mark_aid_B()
+	marked_aid_B = mp.get_property_number("aid", 0)
+	if marked_aid_B == 0
+	then
+		mp.osd_message("当前音轨无效", 1)
+		marked_aid_B = nil
+	else
+		mp.osd_message("预标记当前音轨序列 " .. marked_aid_B .. " 为并行轨B", 1)
+	end
+end
+function mark_aid_merge()
+	if marked_aid_A == marked_aid_B or marked_aid_A == nil or marked_aid_B == nil
+	then
+		mp.osd_message("无效的AB音轨", 1)
+		marked_aid_A, marked_aid_B = nil, nil
+	else
+		mp.command("set lavfi-complex \"[aid" .. marked_aid_A .. "] [aid" .. marked_aid_B .. "] amix [ao]\"")
+		mp.osd_message("已合并AB音轨", 1)
+		merged_aid = true
+	end
+end
+function mark_aid_reset()
+	mp.command("set lavfi-complex \"\"")
+	merged_aid = false
+	marked_aid_A, marked_aid_B = nil, nil
+end
+function mark_aid_fin()
+	if merged_aid then
+		mark_aid_reset()
+		mp.osd_message("已取消并轨和标记", 1)
+		mp.commandv("set", "aid", "auto")
+		return
+	end
+	if marked_aid_A == nil then
+		mark_aid_A()
+		return
+	end
+	if marked_aid_B == nil then
+		mark_aid_B()
+		return
+	end
+	mark_aid_merge()
+end
+
 local shuffled = false
-function playlist_shuffle()
+local shuffling = false
+function show_playlist()
+	mp.add_timeout(0.1, function()
+		local shuffle_msg = mp.command_native({"expand-text", "${playlist}"})
+		shuffling = false
+		mp.osd_message(shuffle_msg, 2)
+	end)
+end
+function playlist_order(mode, re)
+	if shuffling then
+		mp.msg.info("已阻止高频洗牌")
+		return
+	end
 	if mp.get_property_number("playlist-count") <= 2 then
 		mp.osd_message("播放列表中的条目数量不足", 1)
 		return
 	end
-	if not shuffled then
-		mp.command("playlist-shuffle")
-		mp.osd_message(mp.command_native({"expand-text", "${playlist}"}), 2)
-		shuffled = true
-	else
-		mp.command("playlist-unshuffle")
-		mp.osd_message(mp.command_native({"expand-text", "${playlist}"}), 2)
-		shuffled = false
+	shuffling = true
+	if mode == 0 then
+		if not shuffled then
+			mp.command("playlist-shuffle")
+			shuffled = true
+			if re then
+				mp.commandv("playlist-play-index", 0)
+			end
+			show_playlist()
+		else
+			mp.command("playlist-unshuffle")
+			shuffled = false
+			if re then
+				mp.commandv("playlist-play-index", 0)
+			end
+			show_playlist()
+		end
+	elseif mode == 1 then
+		if shuffled then
+			mp.command("playlist-unshuffle")
+			mp.command("playlist-shuffle")
+			if re then
+				mp.commandv("playlist-play-index", 0)
+			end
+			show_playlist()
+		else
+			mp.command("playlist-shuffle")
+			shuffled = true
+			if re then
+				mp.commandv("playlist-play-index", 0)
+			end
+			show_playlist()
+		end
 	end
 end
 
@@ -49,7 +162,7 @@ function quit_real()
 		pre_quit = true
 		mp.add_timeout(1, function()
 			pre_quit = false
-			mp.msg.verbose("检测到误触退出键")
+			mp.msg.verbose("quit_real 检测到误触退出键")
 		end)
 	end
 end
@@ -71,53 +184,43 @@ function quit_wait()
 	end
 end
 
-function seek_auto_back()
-	local current_sid = mp.get_property_number("sid") or 0
+function seek_auto(num)
+	local current_sid = mp.get_property_number("sid", 0)
 	if current_sid == 0 then
-		mp.command("seek " .. -0.1 .. " keyframes")
+		mp.command("seek " .. 0.1 * num .. " keyframes")
 	else
-		mp.command("sub-seek " .. -1 .. "primary")
+		mp.command("sub-seek " .. num .. " primary")
 	end
 end
-function seek_auto_next()
-	local current_sid = mp.get_property_number("sid") or 0
-	if current_sid == 0 then
-		mp.command("seek " .. 0.1 .. " keyframes")
-	else
-		mp.command("sub-seek " .. 1 .. "primary")
-	end
-end
-function seek_skip_back()
+function seek_skip(num)
 	local duration_div_chapters = mp.get_property_number("duration", 1) / mp.get_property_number("chapter-list/count", 1)
 	if duration_div_chapters >= 240 then
-		mp.command("seek " .. -10 .. " relative-percent+exact")
+		mp.command("seek " .. 10 * num .. " relative-percent+exact")
 	elseif duration_div_chapters == 1 then
-		mp.command("seek " .. -60 .. " exact")
+		mp.command("seek " .. 60 * num .. " exact")
 	else
-		mp.commandv("add", "chapter", -1)
-		mp.command("show-progress")
-	end
-end
-function seek_skip_next()
-	local duration_div_chapters = mp.get_property_number("duration", 1) / mp.get_property_number("chapter-list/count", 1)
-	if duration_div_chapters >= 240 then
-		mp.command("seek " .. 10 .. " relative-percent+exact")
-	elseif duration_div_chapters == 1 then
-		mp.command("seek " .. 60 .. " exact")
-	else
-		mp.commandv("add", "chapter", 1)
+		mp.commandv("add", "chapter", num)
 		mp.command("show-progress")
 	end
 end
 
 local bak_speed = nil
 function speed_auto(tab)
-	if tab.event == "up" then
-		mp.commandv("set", "speed", 1)
-		mp.msg.verbose("已恢复常速")
-	elseif tab.event == "down" then
-		mp.commandv("set", "speed", 2)
-		mp.msg.verbose("加速播放中")
+	if tab.event == "down" then
+		mp.set_property_number("speed", 2)
+		mp.msg.verbose("speed_auto 加速播放中")
+	elseif tab.event == "up" then
+		mp.set_property_number("speed", 1)
+		mp.msg.verbose("speed_auto 已恢复常速")
+	end
+end
+function speed_auto_bullet(tab)
+	if tab.event == "down" then
+		mp.set_property_number("speed", 0.5)
+		mp.msg.verbose("speed_auto_bullet 子弹时间中")
+	elseif tab.event == "up" then
+		mp.set_property_number("speed", 1)
+		mp.msg.verbose("speed_auto_bullet 已恢复常速")
 	end
 end
 function speed_recover()
@@ -132,80 +235,98 @@ function speed_recover()
 	end
 end
 
-function trackA_back()
-	mp.command("add aid -1")
-	if mp.get_property_number("aid", 0) == 0 then
-		mp.command("add aid -1")
-		if mp.get_property_number("aid", 0) == 0 then
-			mp.osd_message("无音频轨", 1)
-		end
+local show_page = 0
+function stats_cycle(num_init, num_end)
+	if show_page < num_init then
+		show_page = num_init - 1
 	end
-end
-function trackA_next()
-	mp.command("add aid 1")
-	if mp.get_property_number("aid", 0) == 0 then
-		mp.command("add aid 1")
-		if mp.get_property_number("aid", 0) == 0 then
-			mp.osd_message("无音频轨", 1)
-		end
+	if show_page >= num_end then
+		show_page = num_init - 1
 	end
+	show_page = show_page + 1
+	mp.command("script-binding stats/display-page-" .. show_page)
 end
-function trackS_back()
-	mp.command("add sid -1")
-	if mp.get_property_number("sid", 0) == 0 then
-		mp.command("add sid -1")
-		if mp.get_property_number("sid", 0) == 0 then
-			mp.osd_message("无字幕轨", 1)
-		end
-	end
-end
-function trackS_next()
-	mp.command("add sid 1")
-	if mp.get_property_number("sid", 0) == 0 then
-		mp.command("add sid 1")
-		if mp.get_property_number("sid", 0) == 0 then
-			mp.osd_message("无字幕轨", 1)
-		end
-	end
-end
-function trackV_back()
-	mp.command("add vid -1")
-	if mp.get_property_number("vid", 0) == 0 then
-		mp.command("add vid -1")
-		if mp.get_property_number("vid", 0) == 0 then
-			mp.osd_message("无视频轨", 1)
-		end
-	end
-end
-function trackV_next()
-	mp.command("add vid 1")
-	if mp.get_property_number("vid", 0) == 0 then
-		mp.command("add vid 1")
-		if mp.get_property_number("vid", 0) == 0 then
-			mp.osd_message("无视频轨", 1)
+
+function track_seek(id, num)
+	mp.command("add " .. id .. " " .. num)
+	if mp.get_property_number(id, 0) == 0 then
+		mp.command("add " .. id .. " " .. num)
+		if mp.get_property_number(id, 0) == 0 then
+			mp.osd_message("无可用轨道", 1)
 		end
 	end
 end
 
+local temp_avc = false
+local x264_cmpt_warn = tostring("x264_cmpt 可能破坏常规h264文件的播放")
+function x264_cmpt()
+	local current_vid = mp.get_property_number("vid", 0)
+	if current_vid == 0 then
+		mp.osd_message("当前视频轨道无效", 1)
+		return
+	end
+	if temp_avc then
+		mp.set_property_bool("vd-lavc-assume-old-x264", false)
+		temp_avc = false
+		mp.osd_message("已禁用旧版x264即时兼容模式", 1)
+		mp.set_property_number("vid", 0)
+		mp.set_property_number("vid", current_vid)
+		return
+	end
+	mp.set_property_bool("vd-lavc-assume-old-x264", true)
+	temp_avc = true
+	mp.osd_message("已启用旧版x264即时兼容模式", 1)
+	mp.msg.warn(x264_cmpt_warn)
+	mp.set_property_number("vid", 0)
+	mp.set_property_number("vid", current_vid)
+end
 
+
+--
+-- 事件注册
+--
+
+mp.register_event("end-file", function() if marked_aid_A ~= nil or marked_aid_B ~= nil then mark_aid_reset() end end)
+
+--mp.register_event("end-file", function() if temp_avc then mp.set_property_bool("vd-lavc-assume-old-x264", false) temp_avc = false end end)
+mp.register_event("start-file", function() if temp_avc then mp.msg.warn(x264_cmpt_warn) end end)
+
+
+--
 -- 键位绑定
+--
 
-mp.add_key_binding(nil, "playlist_shuffle", playlist_shuffle)
+mp.add_key_binding(nil, "mark_aid_A", mark_aid_A)
+mp.add_key_binding(nil, "mark_aid_B", mark_aid_B)
+mp.add_key_binding(nil, "mark_aid_merge", mark_aid_merge)
+mp.add_key_binding(nil, "mark_aid_reset", function() mark_aid_reset() mp.osd_message("已取消并轨和标记", 1) end)
+mp.add_key_binding(nil, "mark_aid_fin", mark_aid_fin)
+
+mp.add_key_binding(nil, "playlist_order_0", function() playlist_order(0) end)
+mp.add_key_binding(nil, "playlist_order_0r", function() playlist_order(0, true) end)
+mp.add_key_binding(nil, "playlist_order_1", function() playlist_order(1) end)
+mp.add_key_binding(nil, "playlist_order_1r", function() playlist_order(1, true) end)
 
 mp.add_key_binding(nil, "quit_real", quit_real)
 mp.add_key_binding(nil, "quit_wait", quit_wait)
 
-mp.add_key_binding(nil, "seek_auto_back", seek_auto_back, {repeatable = true})
-mp.add_key_binding(nil, "seek_auto_next", seek_auto_next, {repeatable = true})
-mp.add_key_binding(nil, "seek_skip_back", seek_skip_back)
-mp.add_key_binding(nil, "seek_skip_next", seek_skip_next)
+mp.add_key_binding(nil, "seek_auto_back", function() seek_auto(-1) end, {repeatable = true})
+mp.add_key_binding(nil, "seek_auto_next", function() seek_auto(1) end, {repeatable = true})
+mp.add_key_binding(nil, "seek_skip_back", function() seek_skip(-1) end)
+mp.add_key_binding(nil, "seek_skip_next", function() seek_skip(1) end)
 
 mp.add_key_binding(nil, "speed_auto", speed_auto, {complex = true})
+mp.add_key_binding(nil, "speed_auto_bullet", speed_auto_bullet, {complex = true})
 mp.add_key_binding(nil, "speed_recover", speed_recover)
 
-mp.add_key_binding(nil, "trackA_back", trackA_back)
-mp.add_key_binding(nil, "trackA_next", trackA_next)
-mp.add_key_binding(nil, "trackS_back", trackS_back)
-mp.add_key_binding(nil, "trackS_next", trackS_next)
-mp.add_key_binding(nil, "trackV_back", trackV_back)
-mp.add_key_binding(nil, "trackV_next", trackV_next)
+mp.add_key_binding(nil, "stats_1_2", function() stats_cycle(1, 2) end)
+mp.add_key_binding(nil, "stats_0_4", function() stats_cycle(0, 4) end)
+
+mp.add_key_binding(nil, "trackA_back", function() track_seek("aid", -1) end)
+mp.add_key_binding(nil, "trackA_next", function() track_seek("aid", 1) end)
+mp.add_key_binding(nil, "trackS_back", function() track_seek("sid", -1) end)
+mp.add_key_binding(nil, "trackS_next", function() track_seek("sid", 1) end)
+mp.add_key_binding(nil, "trackV_back", function() track_seek("vid", -1) end)
+mp.add_key_binding(nil, "trackV_next", function() track_seek("vid", 1) end)
+
+mp.add_key_binding(nil, "x264_cmpt", x264_cmpt)
